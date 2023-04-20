25a77d6b497cd335f273b554e4523741b98196b4
graykeng <graykeng7@gmail.com>
2023-04-20 15:16:46 -0700

    k

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         TeamBard
       </div>
       <div>
-        <iframe title="iframe" style={{ width: '100%', height: '100vh', border: 'none' }} src="https://lookerstudio.google.com/embed/reporting/5b7edf5b-fa69-410a-b32a-267a62f84857/page/SRSND" frameborder="0" allowFullScreen></iframe>
+        <iframe title="iframe" style={{ width: '95%', height: '100vh', border: 'none' }} src="https://lookerstudio.google.com/embed/reporting/5b7edf5b-fa69-410a-b32a-267a62f84857/page/SRSND" frameborder="0" allowFullScreen></iframe>
       </div>
 
     </div>
