6ea959af18139419026c115744b77523805d763f
graykeng <graykeng7@gmail.com>
2023-04-20 14:34:31 -0700

    looks good

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>TeamBard</header>
+      <iframe width={900} height={900} src="https://lookerstudio.google.com/embed/reporting/5b7edf5b-fa69-410a-b32a-267a62f84857/page/SRSND" frameborder="0" allowFullScreen></iframe>
+
     </div>
   );
 }
