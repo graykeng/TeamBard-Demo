87a186c1cb42748c64ac73a07f860dce43507cd2
graykeng <graykeng7@gmail.com>
2023-04-20 15:01:48 -0700

    ll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import './App.css'
 
 function App() {
   return (
     <div>
-      <header>TeamBard</header>
-      <iframe title="iframe" onload="this.width=screen.width;this.height=screen.height;" src="https://lookerstudio.google.com/embed/reporting/5b7edf5b-fa69-410a-b32a-267a62f84857/page/SRSND" frameborder="0" allowFullScreen></iframe>
+      <div className="title">
+        TeamBard
+      </div>
+      <div>
+        <iframe title="iframe" style={{ width: '100%', height: '100vh', border: 'none' }} src="https://lookerstudio.google.com/embed/reporting/5b7edf5b-fa69-410a-b32a-267a62f84857/page/SRSND" frameborder="0" allowFullScreen></iframe>
+      </div>
 
     </div>
   );
